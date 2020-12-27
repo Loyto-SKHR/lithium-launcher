48f60ec331995196e7bedf8f97a0e78865169d69
Lucas Avelino <loyto.skhr@gmail.com>
2020-12-27 17:17:16 +0100

    Client Update

--- src/fr/loyto/lithium/launcher/Launcher.java
+++ src/fr/loyto/lithium/launcher/Launcher.java
@@ -12,6 +12,9 @@ import fr.theshark34.openlauncherlib.launcher.GameInfos;
 import fr.theshark34.openlauncherlib.launcher.GameTweak;
 import fr.theshark34.openlauncherlib.launcher.GameType;
 import fr.theshark34.openlauncherlib.launcher.GameVersion;
+import fr.theshark34.supdate.BarAPI;
+import fr.theshark34.supdate.SUpdate;
+import fr.theshark34.swinger.Swinger;
 
 public class Launcher {
 	
@@ -20,11 +23,44 @@ public class Launcher {
 	public static final File LT_DIR = LT_INFOS.getGameDir();
 	
 	private static AuthInfos authInfos;
+	private static Thread updateThread;
 	
 	public static void auth(String username, String password) throws AuthenticationException {
 		Authenticator authenticator = new Authenticator(Authenticator.MOJANG_AUTH_URL, AuthPoints.NORMAL_AUTH_POINTS);
 		AuthResponse response = authenticator.authenticate(AuthAgent.MINECRAFT, username, password, "");
 		authInfos = new AuthInfos(response.getSelectedProfile().getName(), response.getAccessToken(), response.getSelectedProfile().getId());
+	}
+	
+	public static void update() throws Exception {
+		SUpdate su = new SUpdate("https://loyto-skhr.tk/SUpdate/", LT_DIR);
+		
+		updateThread = new Thread() {
+			private int val;
+			private int max;
+			
+			@Override
+			public void run() {
+				while(this.isInterrupted()) {
+					val = (int) (BarAPI.getNumberOfTotalDownloadedBytes() / 1000);
+					max = (int) (BarAPI.getNumberOfTotalBytesToDownload() / 1000);
+					
+					LauncherFrame.getInstance().getLaunchPanel().getProgressBar().setMaximum(max);
+					LauncherFrame.getInstance().getLaunchPanel().getProgressBar().setValue(val);
+					
+					LauncherFrame.getInstance().getLaunchPanel().setInfoText("Téléchrgement des fichiers " +
+							BarAPI.getNumberOfDownloadedFiles() + "/" + BarAPI.getNumberOfFileToDownload() +
+								" " + Swinger.percentage(val, max) + "");
+				}
+			}
+		};
+		updateThread.start();
+		
+		su.start();
+		updateThread.interrupt();
+	}
+	
+	public static void interruptThread() {
+		updateThread.interrupt();
 	}
 
 }
--- src/fr/loyto/lithium/launcher/LauncherPanel.java
+++ src/fr/loyto/lithium/launcher/LauncherPanel.java
@@ -5,14 +5,17 @@ import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import fr.litarvan.openauth.AuthenticationException;
 import fr.theshark34.openlauncherlib.launcher.util.UsernameSaver;
 import fr.theshark34.swinger.Swinger;
+import fr.theshark34.swinger.colored.SColoredBar;
 import fr.theshark34.swinger.event.SwingerEvent;
 import fr.theshark34.swinger.event.SwingerEventListener;
 import fr.theshark34.swinger.textured.STexturedButton;
@@ -30,6 +33,9 @@ public class LauncherPanel extends JPanel implements SwingerEventListener {
 	private STexturedButton playButton = new STexturedButton(Swinger.getResource("play.png"));
 	private STexturedButton quitButton = new STexturedButton(Swinger.getResource("quit.png"));
 	private STexturedButton hideButton = new STexturedButton(Swinger.getResource("hide.png"));
+	
+	private SColoredBar progressBar = new SColoredBar(Swinger.getTransparentWhite(100), Swinger.getTransparentWhite(175));
+	private JLabel infoLabel = new JLabel("Clique sur Play!", SwingConstants.CENTER);
 	
 	public LauncherPanel() {
 		this.setLayout(null);
@@ -61,6 +67,13 @@ public class LauncherPanel extends JPanel implements SwingerEventListener {
 		hideButton.setBounds(907, 2);
 		hideButton.addEventListener(this);
 		this.add(hideButton);
+		
+		progressBar.setBounds(0, 600, 975, 25);
+		this.add(progressBar);
+		
+		infoLabel.setFont(usernameField.getFont());
+		infoLabel.setBounds(0, 570, 975, 30);
+		this.add(infoLabel);
 	}
 	
 	@Override
@@ -81,6 +94,15 @@ public class LauncherPanel extends JPanel implements SwingerEventListener {
 						Launcher.auth(usernameField.getText(), passwordField.getText());
 					} catch(AuthenticationException e) {
 						JOptionPane.showMessageDialog(LauncherPanel.this, "Erreur, impossible de se conecter: " + e.getErrorModel().getErrorMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+						setFieldsEnabled(true);
+						return;
+					}
+					
+					try {
+						Launcher.update();
+					} catch(Exception e) {
+						Launcher.interruptThread();
+						JOptionPane.showMessageDialog(LauncherPanel.this, "Erreur, impossible de mettre le jeu à jour: " + e, "Erreur", JOptionPane.ERROR_MESSAGE);
 						setFieldsEnabled(true);
 						return;
 					}
@@ -109,4 +131,13 @@ public class LauncherPanel extends JPanel implements SwingerEventListener {
 		passwordField.setEnabled(enabled);
 		playButton.setEnabled(enabled);
 	}
+	
+	public SColoredBar getProgressBar() {
+		return progressBar;
+	}
+	
+	public void setInfoText(String text) {
+		infoLabel.setText(text);
+	}
+	
 }
